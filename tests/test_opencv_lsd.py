5265e63ce78ac62e8e15bcd64b972ac1ee5665f7
Iago <iago_h92@hotmail.com>
2024-12-23 16:24:54 +0100

    Using Pillow instead opencv for reading images

--- tests/test_opencv_lsd.py
+++ tests/test_opencv_lsd.py
@@ -1,22 +1,21 @@
 from pathlib import Path
 
 import pyfsg
-import cv2
-
-IMSHOW = False
-
+import numpy as np
+from PIL import Image
 
 def test_opencv_lsd():
     img_path = str(Path(__file__).parent.parent / "images" / "P1080079.jpg")
     print(f"Reading image from {img_path}")
 
     # Load a grayscale image as uint8
-    gray = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    gray = np.asarray(Image.open(img_path).convert('L'))
 
     # 1) Basic usage: just get Nx4 lines
     lines = pyfsg.detectLinesOpencvLSD(gray)
     print("Detected lines:", lines.shape)  # e.g. (150, 4)
-
+    assert lines.ndim == 2
+    assert len(lines) > 1000
     # color = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
     # for line in lines.astype(int):
     #     cv2.line(color, (line[0], line[1]), (line[2], line[3]), (0, 255, 0))
@@ -26,6 +25,8 @@ def test_opencv_lsd():
 
     # 2) Request optional arrays
     res = pyfsg.detectLinesOpencvLSD(gray, return_width=True, return_prec=True, return_nfa=True)
+    assert res["lines"].ndim == 2
+    assert len(res["lines"]) > 1000
 
     print("Detected lines shape:", res["lines"].shape)  # (N,4)
     print("Widths shape:", res["width"].shape)  # (N,)
